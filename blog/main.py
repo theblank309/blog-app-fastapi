14d3280d01edf7a466b9359dcab9cf23a3b75f67
Siddhesh Morye <63141152+theblank309@users.noreply.github.com>
2024-01-07 16:58:14 +0530

    added delete & update

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException, status, Depends
 from sqlalchemy.orm import Session
+import uvicorn
 
-from . import schema, models
+from blog import schema, models
 from blog.database import engine, SessionLocal
 
 models.Base.metadata.create_all(engine)
@@ -23,6 +24,18 @@ def create(request: schema.Blog, db: Session = Depends(getDB)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(getDB)):
+    db.query(models.Blog).where(models.Blog.id == id).delete()
+    db.commit()
+    return "deleted"
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schema.Blog, db: Session = Depends(getDB)):
+    db.query(models.Blog).where(models.Blog.id == id).update({'title': request.title, 'body': request.body})
+    db.commit()
+    return "updated"
+
 @app.get('/blog', status_code=status.HTTP_200_OK)
 def all(db: Session = Depends(getDB)):
     blogs = db.query(models.Blog).all()
@@ -35,3 +48,6 @@ def show(id: int, db: Session = Depends(getDB)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                              detail=f"blog with id {id} not found")
     return blog
+
+if __name__ == "__main__":
+    uvicorn.run("blog.main:app", host="127.0.0.1", port=8000, reload=True)
